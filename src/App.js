a68922c09e94cd9dc7bdbff86453d95669f92e5c
leslietagne <leslietagne7@gmail.com>
2026-04-16 15:46:15 +0200

    Fix: langue persistante LanguageProvider + localStorage

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { LanguageProvider } from './context/LanguageContext';
 import Landing from './pages/Landing';
 import Analyse from './pages/Analyse';
 import Dashboard from './pages/Dashboard';
@@ -8,16 +9,18 @@ import Success from './pages/Success';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/analyse" element={<Analyse />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/auth" element={<Auth />} />
-        <Route path="/pricing" element={<Pricing />} />
-        <Route path="/success" element={<Success />} />
-      </Routes>
-    </Router>
+    <LanguageProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/analyse" element={<Analyse />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/success" element={<Success />} />
+        </Routes>
+      </Router>
+    </LanguageProvider>
   );
 }
 
