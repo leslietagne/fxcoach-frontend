518617be0a768aff2214f26c9de1042a495f10de
leslietagne <leslietagne7@gmail.com>
2026-04-19 00:42:53 +0200

    fix: import path CookieBanner

--- src/components/CookieBanner.jsx
+++ src/components/CookieBanner.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useLang } from '../../context/LanguageContext';
+import { useLang } from '../context/LanguageContext';
 
 export default function CookieBanner() {
   const { lang } = useLang();
